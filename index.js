6c3faa78826d04aba07599f6fb415c6d7aca24c6
SONG <SONG@SONGui-MacBook-Pro.local>
2020-09-04 23:56:53 +0900

    회원가입, 비밀번호암호화,로그인 기능

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,7 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
@@ -29,6 +31,30 @@ app.post("/register", function (req, res) {
     } else {
       return res.status(200).json({ success: true });
     }
+  });
+});
+
+app.post("/login", function (req, res) {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "입력하신 이메일에 해당하는 유저가 없습니다."
+      });
+    }
+    //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+
+      //비밀번호까지 맞다면 토큰을 생성한다.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 쿠키에 저장한다.
+        res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
