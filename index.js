0fdbb2a0f37ec54064fdffbbf39d2025f07e7c22
SONG <SONG@SONGui-MacBook-Pro.local>
2020-09-05 00:57:54 +0900

    Add Auth,Logout

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 const config = require("./config/key");
 const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,7 +23,7 @@ app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-app.post("/register", function (req, res) {
+app.post("/api/users/register", function (req, res) {
   const user = new User(req.body);
 
   user.save(function (err, userInfo) {
@@ -34,7 +35,7 @@ app.post("/register", function (req, res) {
   });
 });
 
-app.post("/login", function (req, res) {
+app.post("/api/users/login", function (req, res) {
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -54,6 +55,30 @@ app.post("/login", function (req, res) {
         // 토큰을 쿠키에 저장한다.
         res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id });
       });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, function (req, res) {
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true 라는 말.
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, //role 0 - 일반 유저 , 1 - 관리자
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  });
+});
+
+app.get("/api/users/logout", auth, function (req, res) {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
     });
   });
 });
