2668f591bf2defd475dc5ec7b3514ae1f820b62e
SONG <SONG@SONGui-MacBook-Pro.local>
2020-09-04 22:34:02 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-const mongoose = require("mongoose");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://admin-jehyuk:asdf1234@cluster0.ksfes.mongodb.net/<dbname>?retryWrites=true&w=majority", //
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
 app.get("/", function (req, res) {
   res.send("hello world");
+});
+
+app.post("/register", function (req, res) {
+  const user = new User(req.body);
+
+  user.save(function (err, userInfo) {
+    if (err) {
+      return res.json({ success: false, err });
+    } else {
+      return res.status(200).json({ success: true });
+    }
+  });
 });
 
 app.listen(port, function () {
