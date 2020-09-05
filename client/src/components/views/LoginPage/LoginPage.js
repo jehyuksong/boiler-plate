b93187619c0b2fdacefc8feaa3062a0118143e93
SONG <SONG@SONGui-MacBook-Pro.local>
2020-09-05 20:14:47 +0900

    Add Register Form

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -29,7 +29,7 @@ function LoginPage(props) {
       if (response.payload.loginSuccess) {
         props.history.push("/");
       } else {
-        alert(Error);
+        alert("Error");
       }
     });
   };
