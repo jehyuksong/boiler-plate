6115da11e967916e6ee24d37ade5d7c9e0a5c37d
SONG <SONG@SONGui-MacBook-Pro.local>
2020-09-05 20:22:11 +0900

    Add Logout Form

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => {
       console.log(response);
     });
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 하는데 실패했습니다.");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +29,8 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
